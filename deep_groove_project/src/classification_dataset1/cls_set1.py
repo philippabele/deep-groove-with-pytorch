1eeeb21b24c60b003c2a549c7c15250292dc52a5
Rico K <85608665+RicoKursidem@users.noreply.github.com>
2023-02-28 13:46:06 +0100

    parameter study
    learning rate, epochs and batch size

--- deep_groove_project/src/classification_dataset1/cls_set1.py
+++ deep_groove_project/src/classification_dataset1/cls_set1.py
@@ -7,18 +7,20 @@ from classification_dataset1.NeuralNetwork import NeuralNetwork
 import matplotlib.pyplot as plt
 
 
-def cls_set1(show=True, params=None):
+def cls_set1(params=None, output={"cli": True, "plot": True}):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = NeuralNetwork().to(device)
-    print(model)
+    if output["cli"]:
+        print(model)
 
     #  Loading and preparing Dataframe
-    dataframe = pd.read_csv("../dataset/dataset1.csv")
+    dataframe = pd.read_csv("../../dataset/dataset1.csv")
 
     # shuffle Dataframe
     dataframe = dataframe.sample(frac=1)
 
-    print(dataframe.head())
+    if output["cli"]:
+        print(dataframe.head())
     dataframe['Label'] = 0
     dataframe['Label'].where(dataframe['Lifetime'] <= 8760, 1, inplace=True)
     dataframe['Label'].where(dataframe['Lifetime'] <= 8760*2, 2, inplace=True)
@@ -26,12 +28,14 @@ def cls_set1(show=True, params=None):
     dataframe['Label'].where(dataframe['Lifetime'] <= 8760*4, 4, inplace=True)
     dataframe['Label'].where(dataframe['Lifetime'] <= 8760*4, 5, inplace=True)
 
-    print(dataframe.head())
+    if output["cli"]:
+        print(dataframe.head())
     # Creating custom Dataset
     train_dataset = FeatureDataset(dataframe[:int(len(dataframe)/2)])
     test_dataset = FeatureDataset(dataframe[int(len(dataframe)/2):])
 
-    print(f"length of Datasets - training: {len(train_dataset)}, test: {len(test_dataset)}")
+    if output["cli"]:
+        print(f"length of Datasets - training: {len(train_dataset)}, test: {len(test_dataset)}")
 
     # Parameters
     if params is None:
@@ -43,37 +47,41 @@ def cls_set1(show=True, params=None):
     # Creating the Dataloaders
     train_dataloader = DataLoader(train_dataset, shuffle=params["shuffle"], batch_size=params["batch_size"])
     test_dataloader = DataLoader(test_dataset, shuffle=params["shuffle"], batch_size=params["batch_size"])
-
-    for batch in train_dataloader:
-        print(f"Batch: {batch[0]}")
-        break
-
-    print(f"LR:  {params['learning_rate']}")
+    if output["cli"]:
+        for batch in train_dataloader:
+            print(f"Batch: {batch[0]}")
+            break
+        print(f"LR:  {params['learning_rate']}")
 
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=params['learning_rate'])
 
     accuracy, avg_loss = [], []
     for e in range(params['epochs']):
-        print(f"Epoch {e + 1}\n-------------------------------")
-        train_loop(train_dataloader, model, loss_fn, optimizer)
-        test_loop(test_dataloader, model, loss_fn, [accuracy, avg_loss])
+        if output["cli"]:
+            print(f"Epoch {e + 1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer, output)
+        test_loop(test_dataloader, model, loss_fn, [accuracy, avg_loss], output)
     print("Done!")
 
-    if show:
+    if output["plot"]:
         show_resultes(accuracy, avg_loss)
     return {"acc": accuracy, "loss": avg_loss}
+
 
 def show_resultes(accuracy, avg_loss):
     plt.plot(accuracy)
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy in %")
     plt.show()
+
     plt.plot(avg_loss)
     plt.xlabel("Epoch")
     plt.ylabel("AVG Loss")
     plt.show()
-def train_loop(dataloader, model, loss_fn, optimizer):
+
+
+def train_loop(dataloader, model, loss_fn, optimizer, output):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
@@ -87,10 +95,11 @@ def train_loop(dataloader, model, loss_fn, optimizer):
 
         if batch % 10 == 0:
             loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>4d}/{size:>4d}]")
+            if output["cli"]:
+                print(f"loss: {loss:>7f}  [{current:>4d}/{size:>4d}]")
 
 
-def test_loop(dataloader, model, loss_fn, charts):
+def test_loop(dataloader, model, loss_fn, charts, output):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
@@ -101,10 +110,10 @@ def test_loop(dataloader, model, loss_fn, charts):
             test_loss += loss_fn(pred, y).item()
 
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    if output["cli"]:
+        test_loss /= num_batches
+        correct /= size
+        print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     charts[0].append((100 * correct))
     charts[1].append(test_loss)
 
--- deep_groove_project/src/classification_dataset1/parameter_study.py
+++ deep_groove_project/src/classification_dataset1/parameter_study.py
@@ -1,13 +1,93 @@
 from cls_set1 import cls_set1
 from cls_set1 import show_resultes
+import matplotlib.pyplot as plt
+import time
+
 
 def main():
-    params = {'batch_size': 10,
+
+    params = {'batch_size': 20,
               'shuffle': True,
-              'learning_rate': 1e-6,
+              'learning_rate': 1e-7,
               'epochs': 200}
-    acc, avg = cls_set1(False, params)
-    show_resultes(acc, avg)
+
+    parameter_batch_size(params, output={"cli": False, "plot": False})
+    parameter_learning_rate(params, output={"cli": False, "plot": False})
+    parameter_epochs(params, output={"cli": False, "plot": False})
+
+
+def parameter_epochs(params, output):
+    st = time.time()
+    values = []
+    epochs = [10, 50, 100, 200, 500, 1000]
+    for i in range(len(epochs)):
+        params["epochs"] = epochs[i]
+        values.append(cls_set1(params, output=output))
+        values[len(values) - 1]['epochs'] = epochs[i]
+
+    elapsed_time = time.time() - st
+    print('Execution time:', elapsed_time, 'seconds')
+
+    for val, i in zip(values, range(len(values))):
+        plt.plot(val["acc"], label=val["epochs"])
+    plt.legend(loc="lower right")
+    plt.title("Accuracy, differing Epochs")
+    plt.show()
+
+    for val, i in zip(values, range(len(values))):
+        plt.plot(val["loss"], label=val["epochs"])
+    plt.legend(loc="upper right")
+    plt.title("AVG Loss, differing Epochs")
+    plt.show()
+
+
+def parameter_learning_rate(params, output):
+    st = time.time()
+    values = []
+    learning_rates = [1e-5, 1e-6, 1e-7, 1e-8, 1e-9]
+    for i in range(len(learning_rates)):
+        params["learning_rate"] = learning_rates[i]
+        values.append(cls_set1(params, output=output))
+        values[len(values) - 1]['learning_rate'] = learning_rates[i]
+
+    elapsed_time = time.time() - st
+    print('Execution time:', elapsed_time, 'seconds')
+
+    for val, i in zip(values, range(len(values))):
+        plt.plot(val["acc"], label=val["learning_rate"])
+    plt.legend(loc="lower right")
+    plt.title("Accuracy, differing Learning Rates")
+    plt.show()
+
+    for val, i in zip(values, range(len(values))):
+        plt.plot(val["loss"], label=val["learning_rate"])
+    plt.legend(loc="upper right")
+    plt.title("AVG Loss, differing Learning Rates")
+    plt.show()
+
+
+def parameter_batch_size(params, output):
+    st = time.time()
+    values = []
+    for i in range(10, 101, 10):
+        params["batch_size"] = i
+        values.append(cls_set1(params, output=output))
+        values[len(values)-1]['batch_size'] = i
+
+    elapsed_time = time.time() - st
+    print('Execution time:', elapsed_time, 'seconds')
+
+    for val, i in zip(values, range(len(values))):
+        plt.plot(val["acc"], label=val["batch_size"])
+    plt.legend(loc="lower right")
+    plt.title("Accuracy, differing Batch Sizes")
+    plt.show()
+
+    for val, i in zip(values, range(len(values))):
+        plt.plot(val["loss"], label=val["batch_size"])
+    plt.legend(loc="upper right")
+    plt.title("AVG Loss, differing Batch Sizes")
+    plt.show()
 
 
 if __name__ == '__main__':
